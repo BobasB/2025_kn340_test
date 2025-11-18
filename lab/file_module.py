e75d2cb1a845bb9bd8be550bed2210f8ff045814
BobasB <bugil.bogdan@gmail.com>
2025-11-18 09:48:14 +0200

    відформатували з black

--- lab/file_module.py
+++ lab/file_module.py
@@ -1,9 +1,19 @@
 # Цей модуль буде відповідати за генерацію слів які потрібно вгадати
 import random
 
-INITIAL_WORDS = ["apple", "banana", "cherry", "orange", 
-                 "Python", "Developer", "function", "variable", 
-                 "iteration", "condition"]
+INITIAL_WORDS = [
+    "apple",
+    "banana",
+    "cherry",
+    "orange",
+    "Python",
+    "Developer",
+    "function",
+    "variable",
+    "iteration",
+    "condition",
+]
+
 
 def get_n_random_words(n: int) -> list:
     if n > len(INITIAL_WORDS):
@@ -19,8 +29,10 @@ def get_n_random_words(n: int) -> list:
         print(f"Генерація {n} випадкових слів.")
     return [w.lower() for w in random.sample(INITIAL_WORDS, n)]
 
+
 def func_for_module_import():
     return "Ця функція призначена для тестування імпорту модуля."
+
 
 def test_func_return_value():
     """
--- lab/tests/test_file_module.py
+++ lab/tests/test_file_module.py
@@ -2,12 +2,14 @@ from unittest.mock import patch
 import pytest
 from lab.file_module import get_n_random_words
 
+
 def test_get_n_random_words():
     """
     Перевіряємо чи функція повертає правильну кількість слів"""
     for n in range(1, 6):
         words = get_n_random_words(n)
         assert len(words) == n, f"Expected {n} words, got {len(words)}"
+
 
 def test_get_n_random_words_raise_value_error():
     """
@@ -17,8 +19,9 @@ def test_get_n_random_words_raise_value_error():
         with pytest.raises(ValueError):
             get_n_random_words(n)
 
+
 def test_get_n_random_words_expect_print_outputs():
-    with patch('builtins.print') as mock_print:
+    with patch("builtins.print") as mock_print:
         for n in range(1, 6):
             get_n_random_words(n)
             mock_print.assert_called_with(f"Генерація {n} випадкових слів.")
